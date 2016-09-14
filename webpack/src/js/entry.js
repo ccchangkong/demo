0700ef66b28a5da3299c631c7cbfe0dc2a0777fe
ccchangkong <chlo0o@qq.com>
2016-09-14 17:53:09 +0800

    add

--- webpack/src/js/entry.js
+++ webpack/src/js/entry.js
@@ -1,9 +1,16 @@
 // entry.js
 
-// javascript
-let People = require('./es6-module.js');
-let p = new People("Yika");
-p.sayHi();
+
 
 // css
-require('../sass/index.scss');
+require('Css');
+require('./es6-module.js');
+console.log('success!');
+// javascript
+
+
+// var People = require('./es6-module.js');
+// // import People from './es6-module.js';
+// var p = new People("Yika");
+// p.sayHi();
+
---
+++ webpack/src/js/es6-module.js
@@ -0,0 +1,11 @@
+// class People{
+//     constructor(name){
+//         this.name = name;
+//     }
+//     sayhi(){
+//         console.log(`hi ${this.name} !`);
+//     }
+// }
+// exports.module = People;
+
+console.log('!!!!!!');
