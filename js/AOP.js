f3a929ae71d99d9d8dddef47fa55142208e490ab
ccchangkong <chlo0o@qq.com>
2016-11-29 22:17:54 +0800

    add

--- js/AOP.js
+++ js/AOP.js
@@ -5,3 +5,11 @@ Function.prototype.before = function(beforefn) {
 		return _self.apply(this, arguments);
 	}
 };
+Function.prototype.after = function(afterfn) {
+	var _self = this;
+	return function () {
+		var ret = _self.apply(this, arguments);
+		afterfn.apply(this, arguments);
+		return ret;
+	}
+};
