00bc6d8baf07ac62adf37a06aeeb049699760977
ccchangkong <chlo0o@qq.com>
2018-11-10 16:03:02 +0800

    add

--- async_all.js
+++ async_all.js
@@ -1,13 +1,32 @@
+// let obj={
+//     strArr:[],
+//     index:0
+// }
+// function result(){
+//     ++obj.index;
+//     if(obj.index==2){
+//         return obj.strArr.join()
+//     }
+// }
+// setTimeout(() => {
+//     obj.strArr[0]='11'
+//     result()
+// }, 1000);
+// setTimeout(() => {
+//     obj.strArr[1]='22'
+//     result()
+// }, 500);
 let obj={
-    strArr:[],
-    index:0
+    strArr:[]
 }
-function result(){
-    ++obj.index;
-    if(obj.index==2){
-        return obj.strArr.join()
+let result=(function (){
+    var $i=0
+    return function (){
+        if(++$i==obj.strArr.length){
+            console.log(obj.strArr.join())
+        }
     }
-}
+})()
 setTimeout(() => {
     obj.strArr[0]='11'
     result()
